a716698911da423ec49cedefd3d09c45e8e5e5ac
Juan Jose Jaramillo <juan.jaramillo14@eia.edu.co>
2026-02-08 00:12:01 -0500

    Se corrigen los CORS para aceptar los datos del front

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-  });
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+});
 
   app.useGlobalPipes(
     new ValidationPipe({
